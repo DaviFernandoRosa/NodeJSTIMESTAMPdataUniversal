0ae7f4d2d3666a3d4c5943854e9cc19de935d3c3
Davi Fernando Rosa <davir0514@gmail.com>
2021-04-08 20:09:58 -0300

    Arquivo index

---
+++ index.js
@@ -0,0 +1,8 @@
+//IMPRIMINDO UMA STRING COM A DATA UNIVERSAL (UTC)
+const hoje = new Date();
+console.log(hoje.toUTCString());
+
+
+//IMPRIMINDO UMA STRING COM A DATA ARMAZENADA
+const hojee = new Date();
+console.log(hojee.toString());
